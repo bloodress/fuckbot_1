de24c743f2d04e85e897d26a76c9ee8c90f1ec93
bloodress <38112207+bloodress@users.noreply.github.com>
2021-07-15 01:13:21 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+import telebot
+bot = telebot.TeleBot('1859408752:AAEH-tV_20WUbdT1lk_GP8KH3H26f-qh3MA')
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, f'Я бот. Приятно познакомиться, {message.from_user.first_name}')
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text.lower() == 'привет':
+        bot.send_message(message.from_user.id, 'Привет!')
+    else:
+        bot.send_message(message.from_user.id, 'Не понимаю, что это значит.')
+bot.polling(none_stop=True)
